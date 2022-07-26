339ee25341d2529c05cae596bf491ee3ced3a81b
Josep Valls <alfredvays@gmail.com>
2022-07-26 18:03:26 +0200

    Empiezo a crear clase para las liguillas de grupos

---
+++ league.js
@@ -0,0 +1,63 @@
+
+//*TODO: Mostrar los equipos inscritos por pantalla.
+const teams = [
+    'ALbalat',
+    'Polinya',
+    'Benicull',
+    'Corbera'
+]
+
+
+
+//Una liga tendrá un conjunto de jornadas
+//Cada Jornada tendrá un conjunto de partidos
+//Un partido es el enfrentamiento entre dos equipos
+//El desarrollo de un partido generará un resultado
+
+class League {
+    constructor(name, teams, config = {}) {
+        this.name = name
+        this.setupTeams(teams)
+        this.setup(config)//Setup con la configuracion por defecto
+        this.matches = []// Partidos
+        this.matchDaySchedule = [] //Planificación de jornadas
+        this.scores = [] // Clasificación
+        
+    }
+
+    setup(config = {}) {
+        const defaultConfig = {
+            pointsPerWin : 3,
+            pointsPerDraw : 1,
+            pointsPerLose : 0
+        }
+        this.config = Object.assign(defaultConfig, config)
+    }
+
+    setupTeams(teams) {
+        this.teams = []
+        for ( let team of teams) {
+            let teamObj = {
+                name: team,
+                matchesWon: 0,
+                matchesDraw: 0,
+                matchesLost: 0,
+                points: 0,
+                goalsFor: 0,
+                goalsAgainst: 0
+            }
+            this.teams.push(teamObj)
+        }
+    }
+}
+
+const groupA = new League('Group A', teams)
+console.log(groupA.teams)
+
+//*TODO: Crear la planificación de jornadas y partidos de cada jornada.
+
+//*TODO: Mostrarla por pantalla.
+//*TODO: Jugar lo partidos de todas las jornadas.
+//Una vez terminada cada jornada, se deberá mostrar cómo queda la clasificación de la misma.
+//*TODO: Una vez terminada la liga, se mostrarán estadísticas de número de goles totales y total de puntos ganados.
+ 
